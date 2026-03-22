ea66fd1bb73e2765b62d5464d32b4534e326aff2
Josh Chalmers <joshthejuggler@hotmail.com>
2026-03-22 14:59:49 -0600

    Fix clipping of 'Download Full Report' tooltip on right edge of employer dashboard by repositioning it and removing redundant title attribute.

--- includes/class-mc-employer-dashboard.php
+++ includes/class-mc-employer-dashboard.php
@@ -888,7 +888,7 @@ class MC_Employer_Dashboard
                                                             <input type="hidden" name="nonce" value="<?php echo wp_create_nonce('mc_culture_scorecard_nonce'); ?>">
                                                             <input type="hidden" name="candidate_id" value="<?php echo $employee_user->ID; ?>">
                                                             <input type="hidden" name="scorecard_id" value="scorecard_1">
-                                                            <button type="submit" class="mc-action-btn mc-tooltip mc-tooltip-left" title="Download Full Assessment Report">
+                                                            <button type="submit" class="mc-action-btn mc-tooltip mc-tooltip-left">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                                                                 </svg>
